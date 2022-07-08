cb99cc041a661b912cffbfd047bc1e6a809a3e26
abccen <imbel3000gmail.com>
2022-07-08 15:21:20 +0600

    added new url to

--- students_app/urls.py
+++ students_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from students_app.views import StudentVievSet
 
 urlpatterns = [
-    path('', StudentVievSet.as_view({'get': 'list'}), name='student-list'),   #as_view если путь написан на class то ее нужно писать
+    path('', StudentVievSet.as_view({'get': 'list', 'post': 'create'}), name='student-list'),   #as_view если путь написан на class то ее нужно писать
+    path('<int:pk>/', StudentVievSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'}), name='student-detail')
 ]
